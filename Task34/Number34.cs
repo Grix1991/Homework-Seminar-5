008a57457496e1f299bea84317be308dd565e1ef
Grisha <micronesia@mail.ru>
2022-11-28 21:49:16 +0300

    Важный файл

--- Task34/Number34.cs
+++ Task34/Number34.cs
@@ -7,3 +7,18 @@
 
 [345, 897, 568, 234] -> 2
 */
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные - ");
+
