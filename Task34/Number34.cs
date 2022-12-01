a2ab9f3e24495384b1e7531fedbdfa3be620ab38
Grisha <micronesia@mail.ru>
2022-12-01 08:43:57 +0300

    Дополнил решение

--- Task34/Number34.cs
+++ Task34/Number34.cs
@@ -8,17 +8,36 @@
 [345, 897, 568, 234] -> 2
 */
 using static Library;
-Console.WriteLine("Введите размер массива");
+
+
+Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
+Console.WriteLine("Получившийся массив: ");
 PrintArray(numbers);
 int count = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
+for (int j = 0; j < numbers.Length; j++)
+if (numbers[j] % 2 == 0)
 count++;
 
-Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные - ");
+Console.WriteLine($"Массив состоит из {numbers.Length} чисел,и {count} из них чётные");
 
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
