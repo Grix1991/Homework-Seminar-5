be571a918a56da8599031c82549976d6f5025beb
Grisha <micronesia@mail.ru>
2022-12-01 09:03:54 +0300

    Заокнчил решение задачи

--- Task36/Number36.cs
+++ Task36/Number36.cs
@@ -7,3 +7,34 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Получившийся массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int k = 0; k < numbers.Length; k = k + 2)
+    sum = sum + numbers[k];
+    
+    Console.WriteLine($"Массив состоит из {numbers.Length} чисел и сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
